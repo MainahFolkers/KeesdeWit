fc1034072b75cb11fc0d767ffe808b7775e4b22d
RianneSchoon <31656891+RianneSchoon@users.noreply.github.com>
2017-12-11 13:04:35 +0100

    Add files via upload
    feedback idea punten ingevoegd: mut_dir() en mut_fold() staan nu niet meer in hill_climber.py maar in Protein_class file! :D
    (ik ben nog bezig met de hillclimber aan te passen. Ook ga ik vandaag verder met de C toevoegen in de class protein (dus daarvoor de score functie aan te passen zodat dat ook meegenomen wordt))

--- hill_climber.py
+++ hill_climber.py
@@ -2,107 +2,65 @@ from Protein_class import *
 from copy import deepcopy
 import csv
 
-# improvement: in different file, determine amount of mutations
-def mut_dir(protein):
-    # randomly choice position in protein to mutate
-    # the first bond cannot be chosen to avoid symmetry
-    mut = choice(range(1, protein.n - 1))
-    # directions a bond can move to (right, down, left, up)
-    direcs = ['r', 'd', 'l', 'u']
-    # remove original direction from options
-    direcs.remove(protein.directions[mut])
-    # random choice 1 of 3 directions
-    direc = choice(direcs)
-    # save mutated direction
-    protein.directions[mut] = direc
-
-    return protein
-
-def mut_fold(protein):
-    # first amino acid starts at x=0 and y = 0
-    x, y = 0, 0
-    # save first coordinates
-    protein.coordinates = [[x, y]]
-    # start at first bond
-    b = 0
-    # iterate over bonds
-    while b < protein.n - 1:
-
-        # calculate next coordinates
-        [nx, ny] = protein.bend(protein.directions[b], x, y)
-
-        # if point on grid is not yet occupied by other amino acid
-        if [nx, ny] not in protein.coordinates:
-            # update old x and y
-            [x, y] = [nx, ny]
-            # new coordinates are safed
-            protein.coordinates.append([x, y])
-
-            # continue with next bond
-            b += 1
-
-        # if folding is invalid
-        elif [nx, ny] in protein.coordinates:
-            return None
-
-    return protein
-
 def hill_climb(protein, ITER):
     # open output file which stores best scores per iteration
     ofile  = open('hill_scores.csv', "w", newline="")
     writer = csv.writer(ofile, delimiter=",")
 
-    # fold protein randomly
+    # randomly fold the protein
     new = protein.rand_fold()
-    # until valid folding
-    while not new:
-        new = protein.rand_fold()
 
+    # this first folding is the only folding, so the current best
     best = deepcopy(new)
-    # first folding will always be an improvement
-    # because minimum score = 0
+
+    # best score = 1 so that even if first mutated fold = 0 so first fold is always improvement
     best.score = 1
 
+    # array to see the score progress
     scores = []
 
-    i = 0
     # improvement: determine how many iterations
-    while i < ITER:
+    while i in range(ITER):
 
         # print each 100th iteration
         if i % (ITER/100) == 0:
             print(i)
 
+        # new folding continues on current best fold
         new = deepcopy(best)
-        # mutate direction
-        new = mut_dir(new)
 
-        # if fold is valid after mutation
-        if mut_fold(new):
-            # set protein on grid
-            new.make_grid()
-            # caluclate score
-            new.calc_score()
-            # add best score to scores array
-            scores.append(best.score)
+        # randomly mutate a direction
+        new = new.mut_dir()
+        # continue mutating and trying to fold until valid folding
+        while not new.mut_fold:
+            new = new.mut_dir()
 
-            # continue to next iteration
-            i += 1
+        # # if fold is valid after mutation
+        # if new.mut_fold():
 
-            if new.score < best.score:
-                print("Climbing!", new.score, "<", best.score)
-                # save new best folding
-                best = deepcopy(new)
+        # when valid mutated fold has been made: place protein on grid
+        new.make_grid()
+        # caluclate score
+        new.calc_score()
+        # add best score to scores array
+        scores.append(best.score)
 
-        # if mutated fold is invalid
-        elif not mut_fold(new):
-            # try to fold again
-            continue
+        # # continue to next iteration
+        # i += 1
+
+        # if score improved, continue climbing with this fold
+        if new.score < best.score:
+            print("Climbing!", new.score, "<", best.score)
+            # save new best fold
+            best = deepcopy(new)
 
     # write scores to output file
     writer.writerow(scores)
     # close output file
     ofile.close()
 
+    # print findings to terminal
     print("Hill climber: Best score = ", best.score)
+
+    # output best protein
     return best
