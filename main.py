2f53c127838cd165a6e6744098ca1f70dc8d855b
MainahFolkers <MainahFolkers@users.noreply.github.com>
2017-12-15 09:30:08 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
-from Protein_class import *
-from random_sampler_MF import *
-# from hill_climber import *
+from random_sampler import *
+from hill_climber import *
+from sim_anneal import *
+from visualize import *
+import matplotlib.pyplot as plt
 
-# amount of iterations
-ITER = 1000
+chain = input("Chain: (HHPHHHPH)")
 
-# short protein declaration
-# beste (tot nu toe) met deze random sequentie: -21(!!) en hij ligt er als volgt bij:
-# [[0, 0], [1, 0], [1, 1], [0, 1], [0, 2], [1, 2], [2, 2], [2, 1], [3, 1], [3, 0], [2, 0]]
-sprotein = Protein("HCCHPCHCPCC")
+protein = Protein(chain)
+
+# ask user for amount of iterations
+ITER = int(input("Amount of interations: "))
+
 # random sampler algorithm
-rand_samp(sprotein, ITER)
-# # hill climber algorithm
-# hill_climb(sprotein, ITER)
+protein = rand_samp(protein, ITER)
 
-# # long protein declaration
-# lprotein = Protein("HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH")
-# # random sampler algorithm
-# rand_samp(lprotein, ITER)
-# # hill climber algorithm
-# # improvement: make > 1 mutations per iteration (14 is best for 50 long protein)
-# hill_climb(lprotein, ITER)
+vis = input("Visualize? (y/n) ")
 
-# # trie met array van directions, bij het maken van de trie gelijk de validiteit van de vouwing testen
-# # depth first, weinig geheugen, recursie is een optie: maxdepth is aantal bindingen
+if vis == 'y':
+    # visualize best random sampled protein
+    visualize(protein)
 
-# # visualisatie met tkinter -> erg ingewikkeld
+# ask user for amount of iterations
+AOM = int(input("Amount of mutations: "))
 
-# # experimentatie: lijn grafiek van gemiddelde per algoritme
+# hill climber algorithm
+protein = hill_climb(protein, ITER, AOM)
+
+vis = input("Visualize? (y/n) ")
+
+if vis == 'y':
+    # visualize best hill climbed protein
+    visualize(protein)
+
+# simmulated annealing algorithm
+protein = sim_anneal(protein, ITER, AOM)
+
+vis = input("Visualize? (y/n) ")
+
+if vis == 'y':
+    # visualize best hill climbed protein
+    visualize(protein)
