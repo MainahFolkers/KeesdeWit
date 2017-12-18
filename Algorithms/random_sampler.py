23b7e8baf3a6b01ed0a2ab05f256d041987f1045
MainahFolkers <MainahFolkers@users.noreply.github.com>
2017-12-18 12:18:04 +0100

    opgeschoonde random sampler

--- Algorithms/random_sampler.py
+++ Algorithms/random_sampler.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 def rand_samp(protein, ITER):
     scores = []
 
-    # fold protein randomly until valid folding
+    # fold protein randomly until valid fold
     new = protein.rand_fold()
 
     # we have only one fold, so that is the current best
@@ -13,12 +13,11 @@ def rand_samp(protein, ITER):
     # set impossible best score (minimum score = 0) so first fold always improves
     best.score = 1
 
-    # improvement: determine how many iterations
     for i in range(ITER):
         # fold protein randomly again
         new = protein.rand_fold()
 
-        # set protein on grid
+        # place protein on grid
         new.make_grid()
         # caluclate score
         new.calc_score()
@@ -29,9 +28,22 @@ def rand_samp(protein, ITER):
         # is score improved
         if new.score < best.score:
             print("Improving!", new.score, "<", best.score)
-            # save new best folding
+            # save current best fold
             best = deepcopy(new)
 
-    print("Random sampler: Best score = ", best.score, "ITER = ", ITER)
+    print("Random sampler: Best score = ", best.score)
 
+    # open output file
+    with open("ALGOS_"protein.chain +".txt", 'a+') as ofile:
+        # write algorithm name as label
+        ofile.write(algo + ',')
+        # write comma seperated scores
+        for score in scores:
+            ofile.write(str(score) + ',')
+        # write new line
+        ofile.write('\n')
+    # close output file
+    ofile.close()
+
+    # output best protein
     return best
