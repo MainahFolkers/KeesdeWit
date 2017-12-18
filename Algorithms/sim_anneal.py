472f60bd8711b7e13774f6cd5d304f6572f7ec8c
MainahFolkers <MainahFolkers@users.noreply.github.com>
2017-12-18 13:08:28 +0100

    sim anneal opgeschoond

--- Algorithms/sim_anneal.py
+++ Algorithms/sim_anneal.py
@@ -3,69 +3,86 @@ from copy import deepcopy
 from random import uniform
 
 def sim_anneal(protein, ITER, AOM, COOL):
+    algo = 'sa'
+
     scores = []
 
-    # fold protein randomly
+    # fold protein randomly until valid fold
     new = protein.rand_fold()
 
-    # this first folding is the only folding, so the current best
+    # first fold is the only folding, so the current best
     best = deepcopy(new)
+    all_time_best = deepcopy(new)
 
     # set impossible best score (minimum score = 0) so first fold always improves
     best.score = 1
-    best_score = 1
+    all_time_best.score = 1
 
-    # in temp: dividing by zero impossible, so start at 1
-    i = 0
-    while i < ITER:
-        # mutate some directions in current  best fold
+    i = 1
+    while i < ITER + 1:
+        # new folding continues on current best fold
         new = deepcopy(best)
+
+        # randomly mutate a number of bonds
         new.mut_dir(AOM)
 
-        # continue mutating and trying to fold until valid folding
+        # if the mutated fold is valid
         if new.mut_fold():
-            # when valid mutated fold has been made: place protein on grid
+            # place protein on grid
             new.make_grid()
             # caluclate score
             new.calc_score()
 
             # save best score per iteration
-            scores.append(best_score)
+            scores.append(best.score)
+
+            # temperature is acceptance probability: linear / hyperbolic cooling schedule
+            # if the cooling schedule is linear
+            if COOL is "linear":
+                # temperature decreases linear
+                temp = float(ITER - i)
+            # if cooling schedule is hyperbolic
+            elif COOL is "hyperbolic":
+                # temperature decreases hyperbolic
+                temp = ITER / i
+
+            # if score improved
+            if new.score < best.score:
+                print("Improving!", new.score, "<", best.score)
+                # save current best fold
+                best = deepcopy(new)
+
+            # if score deteriorated
+            else:
+                # acceptance chance for deteriorated fold
+                chance = uniform(1, ITER)
+                # if chance is below current temperature, accept deteriorated fold
+                if chance < temp:
+                    print("Fold accepted anyway!")
+                    # save current best fold
+                    best = deepcopy(new)
+
+            # if best score improved
+            if best.score < all_time_best.score:
+                # save current all time best fold
+                all_time_best = deepcopy(best)
 
             # continue to next iteration
             i += 1
 
-            # temperature is acceptance probability: linear / hyperbolic cooling schedule
-            if COOL is "linear":
-                temp = (ITER - i) / 100
-            elif COOL is "hyperbolic":
-                temp = 100 / i
-
-            # if score improved, accept new fold as best fold
-            if new.score < best.score:
-                print("Improving!", new.score, "<", best.score)
-                best = deepcopy(new)
-                best_score = deepcopy(best.score)
-            # if score deteriorated, accept new fold according to temperature
-            else:
-                # determine acceptance for this worse score
-                chance = uniform(0, 100)
-                # if chance is below current temperature, accept worse fold
-                if chance < temp:
-                    print("Fold accepted anyway!")
-                    best = deepcopy(new)
-
-            if best_score < all_time_best.score:
-                all_time_best = deepcopy(best)
-
     print("Simulated annealing: Best score = ", all_time_best.score)
 
-    with open("COOL_" protein.chain + "AOM= " + str(AOM) +".txt", 'a+') as ofile:
-        ofile.write(COOL + ',')
+    # open output file
+    with open("ALGOS_"protein.chain +".txt", 'a+') as ofile:
+        # write algorithm name as label
+        ofile.write(algo + ',')
+        # write comma seperated scores
         for score in scores:
             ofile.write(str(score) + ',')
+        # write new line
         ofile.write('\n')
+    # close output file
     ofile.close()
 
-    # output best protein
+    # output all time best protein
     return all_time_best
